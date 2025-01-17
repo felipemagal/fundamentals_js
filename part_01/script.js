eb222e60932697d0563d12ef0287c41e5239aeab
Felipe Magalhães (Magal) <felipe19magal@outlook.com>
2025-01-16 23:28:13 -0300

    Fundamentals_part1
    Values, variables and data types

---
+++ part_01/script.js
@@ -0,0 +1,25 @@
+// Recptulando como declarar uma variável
+
+//let js = 'amazing'
+//if (js === 'amazing') alert('JavaScript is Fun!!');
+
+
+//Values and Variables 
+
+// let country = 'Brazil';
+// let continent = 'South America';
+// let population = '200 million';
+
+// console.log(`Hello! I live in ${country}. ${country} is located in ${continent} and the population of ${country} is ${population} of people`);
+
+
+
+// Data Types
+
+let JavaScriptIsFun = true;
+console.log(JavaScriptIsFun)
+
+console.log(typeof true);
+console.log(typeof JavaScriptIsFun);
+console.log(typeof 30);
+console.log(typeof 'Felipe');
