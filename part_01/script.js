81d9469b39533b66070e20acdeb12d07554a78c6
Felipe Magalhães (Magal) <felipe19magal@outlook.com>
2025-01-20 23:13:21 -0300

    revendo conteúdo

--- part_01/script.js
+++ part_01/script.js
@@ -3,6 +3,7 @@
 //let js = 'amazing'
 //if (js === 'amazing') alert('JavaScript is Fun!!');
 
+//-----------------------------------------------------------------------
 
 //Values and Variables 
 
@@ -12,14 +13,60 @@
 
 // console.log(`Hello! I live in ${country}. ${country} is located in ${continent} and the population of ${country} is ${population} of people`);
 
-
+//------------------------------------------------------------------------
 
 // Data Types
 
-let JavaScriptIsFun = true;
-console.log(JavaScriptIsFun)
+// let JavaScriptIsFun = true;
+// console.log(JavaScriptIsFun)
 
-console.log(typeof true);
-console.log(typeof JavaScriptIsFun);
-console.log(typeof 30);
-console.log(typeof 'Felipe');
+// console.log(typeof true);
+// console.log(typeof JavaScriptIsFun);
+// console.log(typeof 30);
+// console.log(typeof 'Felipe');
+
+//------------------------------------------------------------------------
+
+//LET, VAR E CONST
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1994;
+
+// birthYear = 1995
+
+
+// const ageFelipe = 2040 - 1994;
+// console.log(ageFelipe);
+
+// Comparison Operators
+
+// const now = 2037;
+// const felipeAge = now - 1994;
+// const daniAge = now - 1995;
+
+// console.log(felipeAge, daniAge);
+
+// console.log(felipeAge < daniAge);
+// console.log(felipeAge >= 18);
+
+// const fullAge = daniAge;
+
+// console.log(now - 1994 > now - 1994);
+
+
+//------------------------------------------------------------------------
+
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const markBMI = massMark / heightMark **2;
+const johnBMI = massJohn / (heightJohn * heightJohn);
+
+console.log(markBMI, johnBMI);
+
+console.log(markBMI >= johnBMI);
