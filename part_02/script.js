925a6d7679f4617deab993e8e44690b9f395176b
Felipe Magalhães (Magal) <felipe19magal@outlook.com>
2025-02-13 22:41:56 -0300

    Function
    fundamentos de funções

--- part_02/script.js
+++ part_02/script.js
@@ -1,4 +1,25 @@
-'use strict';
+'use strict'; //modo estrito nos proíbe de fazer certas coisas criando erros visíveis
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if(passTest) hasDriverLicense = true;
+// if(hasDriversLicense) console.log('I can drive')
+
+
+// function logger() {
+//     console.log('My name is Felipe');
+// };
+
+// logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+} 
+
+const appleJuice = fruitProcessor(5,0);
+console.log(appleJuice)
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice)
