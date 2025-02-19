ad5f9a146b00898f452f24915b6af16fb249f41c
Felipe Magalhães (Magal) <felipe19magal@outlook.com>
2025-02-18 22:37:19 -0300

    Function Declaration e Function Expression

--- part_02/script.js
+++ part_02/script.js
@@ -13,13 +13,43 @@
 
 // logger();
 
-function fruitProcessor(apples, oranges) {
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
-} 
+// function fruitProcessor(apples, oranges) {
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// } 
 
-const appleJuice = fruitProcessor(5,0);
-console.log(appleJuice)
+// const appleJuice = fruitProcessor(5,0);
+// console.log(appleJuice)
 
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice)
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice)
+
+//
+
+//Function Declaration
+
+// function calAge1(birthYear) {
+//     return 2037- birthYear;
+// };
+
+// const age1 = calAge1(1991)
+// console.log(age1)
+
+
+// //Function Expression
+
+// const calAge2 = function (birthYear){
+//     return 2037 - birthYear
+// }
+
+// const age2 = calAge2(1991);
+// console.log(age1, age2)
+
+
+//Execício 1 - Função Soma
+
+function soma(a,b){
+    return a + b
+}
+
+console.log(soma(2, 5))
